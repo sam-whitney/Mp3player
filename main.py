8b347871a7815f8a8d349632e5924ed764fe1e7e
sam-whitney <sambwhit@gmail.com>
2020-11-21 08:41:48 -1000

    Renamed Audio Files corrected some errors in main file

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
-import playsound
+
 import pygame
 from pygame.locals import *
 from pydub import AudioSegment
 from pydub.playback import play
 
-
-
-
+# initialize pygame for the test key presses
 pygame.init()
-pygame.font.init() # you have to call this at the start,
-                   # if you want to use this module.
+pygame.font.init()
 
 width, height = 64*10, 64*8
-screen=pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height))
 # infinite loop
 
 file1 = "OLDRADIOSHOWS/Abbott_&_Costello_42-10-15_Bank_Robbery.mp3"
 
 
 while True:
-
-
 
     pressed_keys = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -31,10 +26,11 @@ while True:
             pygame.quit()
             exit(0)
         if event.type == pygame.KEYDOWN:
-            if event.key==K_1:
+            if event.key == K_1:
                 song = AudioSegment.from_mp3(file1)
                 play(song)
-            elif event.key==K_2:
-                playsound("OLDRADIOSHOWS/Abbott_&_Costello_42-10-15_Bank_Robbery.mp3")
+            elif event.key == K_ESCAPE:
+                pygame.quit()
+                exit(0)
 
         break
